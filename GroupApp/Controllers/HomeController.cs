ef36b802bc691654328d4728ff95cd49f1b935b8
Sergio <Sergeagui@gmail.com>
2019-10-30 10:07:27 -0500

    Privacy method not used and removed

--- GroupApp/Controllers/HomeController.cs
+++ GroupApp/Controllers/HomeController.cs
@@ -26,11 +26,6 @@ namespace GroupApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
